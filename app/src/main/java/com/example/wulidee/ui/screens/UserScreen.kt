3bf77f3abcd58e14b8c4e4657bc51e01cbbb4836
HuberSimon <simon.huber@st.oth-regensburg.de>
2025-04-10 19:59:15 +0200

    update colors in user and pin lock screen

--- app/src/main/java/com/example/wulidee/ui/screens/UserScreen.kt
+++ app/src/main/java/com/example/wulidee/ui/screens/UserScreen.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.wulidee.data.local.User
 import com.example.wulidee.ui.UserViewModel
